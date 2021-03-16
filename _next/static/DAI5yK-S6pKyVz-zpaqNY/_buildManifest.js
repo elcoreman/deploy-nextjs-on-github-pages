ddba71b360c5344d405ce608b0021daaf090ba76
elcoreman <farshadpour@hotmail.com>
2021-03-16 23:09:31 +0000

    Deploying to gh-pages from @ elcoreman/deploy-nextjs-on-github-pages@bc6a77636900a92d54939630df58dbb380b73e32 🚀

--- _next/static/DAI5yK-S6pKyVz-zpaqNY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-e53e6e413ce4529d554a.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-6038b037292f2a481dca.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/a4HcsIPaHNtUZjOrBg9UZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-8b139d0a857a83d4f703.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-136c2678974ed07b548c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
